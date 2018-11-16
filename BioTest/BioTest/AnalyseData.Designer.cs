9afc54f6d9c7242a0582b8f16c0b35bb2efe11de
HybridRanger <hybridranger64@gmail.com>
2018-11-16 14:54:44 +0000

    heatmap started

--- BioTest/BioTest/AnalyseData.Designer.cs
+++ BioTest/BioTest/AnalyseData.Designer.cs
@@ -29,11 +29,18 @@
         private void InitializeComponent()
         {
             this.Return_To_Menu = new System.Windows.Forms.Button();
+            this.Grid = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
             this.SuspendLayout();
             // 
             // Return_To_Menu
             // 
-            this.Return_To_Menu.Location = new System.Drawing.Point(105, 119);
+            this.Return_To_Menu.Location = new System.Drawing.Point(12, 12);
             this.Return_To_Menu.Name = "Return_To_Menu";
             this.Return_To_Menu.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.Return_To_Menu.Size = new System.Drawing.Size(75, 23);
@@ -42,15 +49,58 @@
             this.Return_To_Menu.UseVisualStyleBackColor = true;
             this.Return_To_Menu.Click += new System.EventHandler(this.Return_To_Menu_Click_1);
             // 
+            // Grid
+            // 
+            this.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.Grid.Location = new System.Drawing.Point(93, 12);
+            this.Grid.Name = "Grid";
+            this.Grid.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.Grid.Size = new System.Drawing.Size(1165, 638);
+            this.Grid.TabIndex = 2;
+            this.Grid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Column4";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Column5";
+            this.Column5.Name = "Column5";
+            // 
             // AnalyseData
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(1270, 662);
+            this.Controls.Add(this.Grid);
             this.Controls.Add(this.Return_To_Menu);
             this.Name = "AnalyseData";
             this.Text = "AnalyseData";
             this.Load += new System.EventHandler(this.AnalyseData_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Grid)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,5 +108,11 @@
         #endregion
 
         private System.Windows.Forms.Button Return_To_Menu;
+        private System.Windows.Forms.DataGridView Grid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
     }
 }
--- BioTest/BioTest/AnalyseData.cs
+++ BioTest/BioTest/AnalyseData.cs
@@ -19,13 +19,24 @@ namespace BioTest
 
         private void AnalyseData_Load(object sender, EventArgs e)
         {
-
+            DataGridViewRow row = (DataGridViewRow)Grid.Rows[0].Clone();
+            for (int i = 0; i < 4; i++)
+            {
+                row.Cells[0].Style.BackColor = Color.Red;
+                row.Cells[1].Value = 50.2;
+            }
+            Grid.Rows.Add(row);
         }
 
         private void Return_To_Menu_Click_1(object sender, EventArgs e)
         {
             this.Owner.Show();
             this.Close();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
