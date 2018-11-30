eb1021b5f1abfa8a05f5a25b5a0b61408d79639b
HybridRanger <hybridranger64@gmail.com>
2018-11-30 14:43:15 +0000

    More Heatmap stuff
    Cell size scales
    Load from data

--- BioTest/BioTest/AnalyseData.Designer.cs
+++ BioTest/BioTest/AnalyseData.Designer.cs
@@ -30,11 +30,7 @@
         {
             this.Return_To_Menu = new System.Windows.Forms.Button();
             this.Grid = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Refresh_Grid = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.Grid)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,49 +48,30 @@
             // Grid
             // 
             this.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.Grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Column1,
-            this.Column2,
-            this.Column3,
-            this.Column4,
-            this.Column5});
-            this.Grid.Location = new System.Drawing.Point(93, 12);
+            this.Grid.Location = new System.Drawing.Point(213, 12);
             this.Grid.Name = "Grid";
+            this.Grid.RowHeadersWidth = 10;
             this.Grid.ScrollBars = System.Windows.Forms.ScrollBars.None;
-            this.Grid.Size = new System.Drawing.Size(1165, 638);
+            this.Grid.Size = new System.Drawing.Size(800, 600);
             this.Grid.TabIndex = 2;
             this.Grid.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
-            // Column1
+            // Refresh_Grid
             // 
-            this.Column1.HeaderText = "Column1";
-            this.Column1.Name = "Column1";
-            // 
-            // Column2
-            // 
-            this.Column2.HeaderText = "Column2";
-            this.Column2.Name = "Column2";
-            // 
-            // Column3
-            // 
-            this.Column3.HeaderText = "Column3";
-            this.Column3.Name = "Column3";
-            // 
-            // Column4
-            // 
-            this.Column4.HeaderText = "Column4";
-            this.Column4.Name = "Column4";
-            // 
-            // Column5
-            // 
-            this.Column5.HeaderText = "Column5";
-            this.Column5.Name = "Column5";
+            this.Refresh_Grid.Location = new System.Drawing.Point(12, 68);
+            this.Refresh_Grid.Name = "Refresh_Grid";
+            this.Refresh_Grid.Size = new System.Drawing.Size(75, 23);
+            this.Refresh_Grid.TabIndex = 3;
+            this.Refresh_Grid.Text = "Refresh";
+            this.Refresh_Grid.UseVisualStyleBackColor = true;
+            this.Refresh_Grid.Click += new System.EventHandler(this.Refresh_Grid_Click);
             // 
             // AnalyseData
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1270, 662);
+            this.Controls.Add(this.Refresh_Grid);
             this.Controls.Add(this.Grid);
             this.Controls.Add(this.Return_To_Menu);
             this.Name = "AnalyseData";
@@ -109,10 +86,6 @@
 
         private System.Windows.Forms.Button Return_To_Menu;
         private System.Windows.Forms.DataGridView Grid;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Button Refresh_Grid;
     }
 }
--- BioTest/BioTest/AnalyseData.cs
+++ BioTest/BioTest/AnalyseData.cs
@@ -7,11 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace BioTest
 {
     public partial class AnalyseData : Form
     {
+        private float min = 0, max = 13;
+        private int numberOfColours;
+        private int cellHeight, cellWidth;
+        private int gridHeight, gridWidth;
+        private int numberOfColumns = 10, numberOfRows = 10;
+        private StreamReader sr = new StreamReader(@"H:\Documents\GitHub\Challenge_BioinformaticsProject_Repo\BioTest\Data.csv");
+        private Dictionary<int, Color> colourDictionary = new Dictionary<int, Color>();
+
         public AnalyseData()
         {
             InitializeComponent();
@@ -19,13 +28,73 @@ namespace BioTest
 
         private void AnalyseData_Load(object sender, EventArgs e)
         {
-            DataGridViewRow row = (DataGridViewRow)Grid.Rows[0].Clone();
-            for (int i = 0; i < 4; i++)
+            gridHeight = Grid.Height;
+            gridWidth = Grid.Width;
+
+            colourDictionary.Add(0, Color.FromArgb(107, 1, 24));
+            colourDictionary.Add(1, Color.FromArgb(182, 22, 37));
+            colourDictionary.Add(2, Color.FromArgb(224, 94, 72));
+            colourDictionary.Add(3, Color.FromArgb(255, 171, 134));
+            colourDictionary.Add(4, Color.FromArgb(254, 221, 204));
+            colourDictionary.Add(5, Color.FromArgb(255, 255, 255));
+            colourDictionary.Add(6, Color.FromArgb(216, 241, 251));
+            colourDictionary.Add(7, Color.FromArgb(153, 205, 232));
+            colourDictionary.Add(8, Color.FromArgb(67, 152, 206));
+            colourDictionary.Add(9, Color.FromArgb(30, 103, 167));
+            colourDictionary.Add(10, Color.FromArgb(16, 47, 91));
+
+            numberOfColours = colourDictionary.Count;
+        }
+
+        private void Refresh_Grid_Click(object sender, EventArgs e)
+        {
+            Grid.ColumnCount = numberOfColumns;
+
+            using (sr)
             {
-                row.Cells[0].Style.BackColor = Color.Red;
-                row.Cells[1].Value = 50.2;
+                DataGridViewRow gridRow = (DataGridViewRow)Grid.Rows[0].Clone();
+
+                string file = sr.ReadToEnd();
+
+                string[] lines = file.Split('\n');
+
+                cellHeight = gridHeight / numberOfRows;
+                cellWidth = gridWidth / numberOfColumns;
+
+                for (int i = 0; i < numberOfRows; i++)
+                {
+                    string line = lines[i];
+                    gridRow = (DataGridViewRow)Grid.Rows[i].Clone();
+
+                    string[] lineElements = line.Split(',');
+
+                    for (int j = 0; j < numberOfColumns; j++)
+                    {
+                        string element = lineElements[j];
+
+                        if (i == 0 && j == 0)
+                        {
+                            Grid.TopLeftHeaderCell.Value = element;
+                        }
+                        else if (j == 0 && i != 0)
+                        {
+                            Grid.Rows[i].HeaderCell.Value = element;
+                            Grid.Rows[i].Height = cellHeight;
+                        }
+                        else if (i == 0 && j != 0)
+                        {
+                            Grid.Columns[j].Width = cellWidth;
+                        }
+                        else
+                        {
+                            Grid.Columns[j].Width = cellWidth;
+                            gridRow.Cells[j].Style.BackColor = ValueToColour(element);
+                        }
+
+                    }
+                    Grid.Rows.Add(gridRow);
+                }
             }
-            Grid.Rows.Add(row);
         }
 
         private void Return_To_Menu_Click_1(object sender, EventArgs e)
@@ -37,6 +106,23 @@ namespace BioTest
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private Color ValueToColour(string stringValue)
+        {
+
+            float floatValue = float.Parse(stringValue, System.Globalization.CultureInfo.InvariantCulture);
+            float step = (max-min)/numberOfColours;
+
+            for (float lower = min, count = 0; lower+step <= max; lower += step, count++)
+            {
+                float upper = lower + step;
+                if (floatValue > lower && floatValue < upper)
+                {
+                    return colourDictionary[Convert.ToInt32(count)];
+                }
+            }
+            return Color.Lime;
         }
     }
 }
